a344d59bd014cce5c152f9ba62d3fd1fc530fcbc
gonza lopardo <glopardob@gmail.com>
2026-03-30 16:46:11 -0300

    Avances en header

---
+++ pi-react/src/Components/Buscador/Buscador.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Formulario extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { valor: '' };
+  }
+
+  evitarSubmit(event) {
+    event.preventDefault();
+  }
+
+  controlarCambios(event) {
+    this.setState({ valor: event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.evitarSubmit(event)}> Buscar...
+        <input type="text" onChange={(event) => this.controlarCambios(event)} value={this.state.valor}/>
+      </form>
+    );
+  }
+}
+
+export default Formulario
---
+++ pi-react/src/Components/Header-Footer/Header.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Buscador from "./Components/Buscador/Buscador"
+
+function Header() {
+  Etiquetas = [
+    {nombre : "Home", link : "index.html"},
+    {nombre : "Peliculas" , link : "movies.html"},
+    {nombre : "Series" , link : "series.html"},
+    {nombre : "Favoritas" , link : "favorites.html"},
+    {nombre : "Registro" , link : "register.html"},
+    {nombre : "Login" , link : "login.html"}]
+
+  Etiquetas2 = [
+    {imagen : "https://image.tmdb.org/t/p/w500/tzrJulItjttxzoX0t3B2My46TS7.jpg",
+     title : "The Thursday Murder Club",
+     text : "A group of senior sleuths passionate about solving cold cases get plunged intoa real-life murder mystery in this comic crime caper.",
+     emote : "🩶"
+     },
+     {imagen : "https://image.tmdb.org/t/p/w500/9PXZIUsSDh4alB80jheWX4fhZmy.jpg",
+     title : "F1",
+     text : "Racing legend Sonny Hayes is coaxed out of retirement to lead a struggling Formula 1 team—and mentor a young hotshot driver—while chasing one more chance at glory.",
+     emote : "♥️"
+     },
+     {imagen : "https://image.tmdb.org/t/p/w500/A06yXys3hrCWu8xiNoHCFLTG5SH.jpg",
+     title : "I Know What You Did Last Summer",
+     text : "When five friends inadvertently cause a deadly car accident, they cover up their involvement and make a pact to keep it a secret rather than face the consequences. A year later, their past comes back to haunt them and they're forced to confront a horrifying truth: someone knows what they did last summer…and is hell-bent on revenge.",
+     emote : "♥️"
+     }, 
+     {imagen : "https://image.tmdb.org/t/p/w500/ombsmhYUqR4qqOLOxAyr5V8hbyv.jpg",
+     title : "Superman",
+     text : "Superman, a journalist in Metropolis, embarks on a journey to reconcile his Kryptonian heritage with his human upbringing as Clark Kent.",
+     emote : "♥️"
+     }
+  ]
+  return (
+    <div className="container">
+      <h1>UdeSA Movies</h1>
+      <nav>
+        <ul className="nav nav-tabs my-4">
+          {Etiquetas.map( item => {
+          <li className="nav-item">
+              <a className="nav-link" href = {item.link} >{item.nombre}</a> 
+          </li>
+          })}
+        </ul>
+      </nav>
+      <Buscador/>
+      <h2 className="alert alert-primary">Popular movies this week</h2>
+     <section className="row cards" id="movies">
+      {Etiquetas2.map(item=>{
+        <article class="single-card-movie">
+            <img src={item.imagen} class="card-img-top" alt="..."/>
+            <div class="cardBody">
+                <h5 class="card-title">{item.title}</h5>
+                <p class="card-text"> {item.text}</p>
+                <a href="movie.html" class="btn btn-primary">Ver más</a>
+                <a href="" class="btn alert-primary">{item.emote}</a>
+            </div>
+        </article>
+      })}
+        
+      </section>
+
+    </div>
+
+    );
+}
