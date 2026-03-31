7b9326f74412eb1af4d5c4ee2954253c629c1e6a
lhermida-prog <lhermida@udesa.edu.ar>
2026-03-31 13:57:13 -0300

    Corregí un par de cosas, aunque faltan algunas otras

--- pi-react/src/App.js
+++ pi-react/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"; 
-import Home from "./components/screens/Home/Home";
-import {Route , Switch} from "react-router-dom"
+import Home from "./screens/Home/Home"
+import { Route, Switch } from 'react-router-dom';
 import Login from "./components/screens/Login/Login"
 import Notfound from "./Components/Not-Found/Not-Found";
 
@@ -13,8 +13,8 @@ function App() {
     <React.Fragment>
       <Header />
       <Switch>
-        <Route path = '/login.html' component = {Login}/>
-        <Route path = '/' component = {Home}/>
+        <Route path = '/login.js' component = {Login}/>
+        <Route path = '/' exact={true} component = {Home}/>
         <Route path = '*' component = {Notfound}/>
       </Switch>  
       <Footer/>
